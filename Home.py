240943a3d4c56e536388b674de8fa99a4b3022c3
cvalle2024 <164565389+cvalle2024@users.noreply.github.com>
2025-07-16 16:36:32 -0600

    Create Home.py

---
+++ Home.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Centro ERSI", layout="centered")
+st.title("👋 Bienvenido al Generador ERSI")
+st.markdown("Selecciona una herramienta desde el menú lateral:")
+st.markdown("- 🧾 Generar Código ERSI")
+st.markdown("- 🔐 Generar Código QR")
