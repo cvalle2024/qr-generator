bb710b04c392154311bed3a0c9d7ba4a43f6c926
cvalle2024 <164565389+cvalle2024@users.noreply.github.com>
2025-07-17 11:29:28 -0600

    Update 1_Generador_Código_ERSI.py

--- pages/1_Generador_Código_ERSI.py
+++ pages/1_Generador_Código_ERSI.py
@@ -9,8 +9,9 @@ from datetime import datetime
 # === CONFIGURACIÓN DE ACCESO A GOOGLE SHEETS ===
 scope = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
 creds = Credentials.from_service_account_info(
-    json.loads(st.secrets["google_sheets"]["gcp_service_account"]),
+    st.secrets["google_service_account"],
     scopes=scope
+
 )
 client = gspread.authorize(creds)
 
