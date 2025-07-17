d05c9d63b263b0e78d5262f62e9e7da9ed99e82c
cvalle2024 <164565389+cvalle2024@users.noreply.github.com>
2025-07-17 09:22:51 -0600

    Update 1_Generador_Código_ERSI.py

--- pages/1_Generador_Código_ERSI.py
+++ pages/1_Generador_Código_ERSI.py
@@ -15,15 +15,17 @@ clave_iniciales = "iniciales_input"
 clave_dia = "dia_input"
 clave_mes = "mes_input"
 clave_sexo = "sexo_input"
+clave_edad="edad_input"
 
 # === Formulario ===
 with st.form("ersi_formulario"):
-    iniciales = st.text_input("Iniciales del Nombre (ej. LMOC)", key=clave_iniciales)
+    iniciales = st.text_input("Iniciales del Nombre y Apellido (ej. LMOC)", key=clave_iniciales)
     dia = st.number_input("Día de nacimiento", min_value=1, max_value=31, step=1, key=clave_dia)
     mes = st.selectbox("Mes de nacimiento",
                        ["ene", "feb", "mar", "abr", "may", "jun", "jul", "ago", "sep", "oct", "nov", "dic"],
                        key=clave_mes)
     sexo = st.selectbox("Sexo", ["Hombre", "Mujer"], key=clave_sexo)
+    edad=st.number_input("Edad", min_value=15, max_value=100, step=1)
 
     generar = st.form_submit_button("Generar Código ERSI")
 
@@ -32,7 +34,7 @@ if generar:
     if iniciales and sexo and dia and mes:
         dia_str = f"{int(dia):02}"
         mes_upper = mes.upper()
-        sexo_code = "HO" if sexo == "Hombre" else "MU"
+        sexo_code = "H" if sexo == "Hombre" else "M"
 
         base = f"{iniciales.upper()}{dia_str}{mes_upper}{sexo_code}"
         ocurrencias = sum(1 for reg in st.session_state["registro"] if base in reg["Código ERSI Base"])
