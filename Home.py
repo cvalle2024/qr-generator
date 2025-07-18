67440dbfdfc8f92331d0287f0928d37e5351f238
cvalle2024 <164565389+cvalle2024@users.noreply.github.com>
2025-07-18 13:52:33 -0600

    Update Home.py

--- Home.py
+++ Home.py
@@ -8,7 +8,7 @@ st.write("Seleccione una opción:")
 col1, col2 = st.columns(2)
 
 with col1:
-    if st.button("🧾 Generar código único para reclutadores"):
+    if st.button("🧾 Generar código único para Reclutadores"):
         st.switch_page("pages/1_Generador_Código_ERSI.py")
 
 with col2:
