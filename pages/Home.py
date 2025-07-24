8acc02969ddaf99184241d8a13b12d9e6701e078
cvalle2024 <164565389+cvalle2024@users.noreply.github.com>
2025-07-24 15:14:34 -0600

    Add files via upload

--- pages/Home.py
+++ pages/Home.py
@@ -1,19 +1,16 @@
 import streamlit as st
 
-if "autenticado" not in st.session_state or not st.session_state["autenticado"]:
-    st.error("🔒 No autorizado. Por favor inicie sesión desde la página principal.")
-    st.stop()
-
-
-# Configurar página
 st.set_page_config(page_title="Home")
 
-# Contenido principal
+# Verificar autenticación
+if "autenticado" not in st.session_state or not st.session_state["autenticado"]:
+    st.error("🔒 No autorizado. Por favor inicie sesión.")
+    st.stop()
+
 st.title("📲 Bienvenido al generador de códigos únicos de identificación para Reclutadores y creación de QR")
 st.write("Seleccione una opción:")
 
 col1, col2 = st.columns(2)
-
 with col1:
     if st.button("🧾 Generar Código ERSI"):
         st.switch_page("1_Generador_Código_ERSI")
@@ -21,4 +18,3 @@ with col1:
 with col2:
     if st.button("🔐 Generar Código QR"):
         st.switch_page("2_Generador_Código_QR")
-
