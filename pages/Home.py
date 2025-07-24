945b6959a22ea5d24744d945af5b73e2d01783a0
cvalle2024 <164565389+cvalle2024@users.noreply.github.com>
2025-07-24 14:33:31 -0600

    Update Home.py

--- pages/Home.py
+++ pages/Home.py
@@ -1,6 +1,11 @@
 import streamlit as st
 
-st.switch_page("Centro ERSI")
+# Proteger con sesión
+if "autenticado" not in st.session_state or not st.session_state["autenticado"]:
+    st.switch_page("Login")  # Esto sí es válido si Login.py está en /pages/
+
+# Configurar página
+st.set_page_config(page_title="Home", layout="centered")
 
 st.title("📲 Bienvenido al generador de códigos únicos de identificación para Reclutadores y creación de QR")
 st.write("Seleccione una opción:")
@@ -9,8 +14,8 @@ col1, col2 = st.columns(2)
 
 with col1:
     if st.button("🧾 Generar Código ERSI"):
-        st.switch_page("pages/1_Generador_Código_ERSI.py")
+        st.switch_page("1_Generador_Código_ERSI")  # nombre visible en el menú lateral
 
 with col2:
     if st.button("🔐 Generar Código QR"):
-        st.switch_page("pages/2_Generador_Código_QR.py")
+        st.switch_page("2_Generador_Código_QR")
