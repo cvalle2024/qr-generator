49c8b51677044bc18a8ef73a8b8e76a2074aaa2e
cvalle2024 <164565389+cvalle2024@users.noreply.github.com>
2025-07-24 13:59:51 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+# app.py (el archivo que se ejecutará primero)
+import streamlit as st
+
+# Inicializa sesión
+if "autenticado" not in st.session_state:
+    st.session_state["autenticado"] = False
+
+# Redirige al login si no ha iniciado sesión
+if not st.session_state["autenticado"]:
+    st.switch_page("pages/Login")
+else:
+    st.switch_page("Home")
