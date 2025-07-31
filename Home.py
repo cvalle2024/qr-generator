20b90197a370d436d484a6d3287d8c41fe8443f2
cvalle2024 <164565389+cvalle2024@users.noreply.github.com>
2025-07-30 18:28:54 -0600

    Update Home.py

--- Home.py
+++ Home.py
@@ -71,7 +71,7 @@ elif st.session_state.verificado:
     with col2:
         if st.button("🔐 Generar Código QR"):
             st.switch_page("pages/2_Generador_Código_QR.py")
-
+    st.warning("⚠️ Debe descargar la tabla antes de continuar.")
     if st.button("Cerrar sesión"):
         st.session_state.clear()
         st.rerun()
