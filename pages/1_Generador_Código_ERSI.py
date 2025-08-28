ea36eff995ae8851826981f885bca73c89cdf8ae
cvalle2024 <164565389+cvalle2024@users.noreply.github.com>
2025-08-27 19:20:35 -0600

    Update 1_Generador_Código_ERSI.py

--- pages/1_Generador_Código_ERSI.py
+++ pages/1_Generador_Código_ERSI.py
@@ -172,7 +172,7 @@ if generar:
 
         sufijo = f"{siguiente_numero:03}"
         codigo_ersi = f"{base}-{sufijo}"
-        fecha_registro = datetime.now().strftime("%Y-%m-%d")
+        fecha_registro = datetime.now().strftime("%d-%m-%Y")
 
         # === GUARDAR DATOS ===
         nuevo = {
